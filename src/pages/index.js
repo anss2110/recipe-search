f99448452e389ef9a77ee75d519233a6a84cc650
Mofumofu <69349170+enomoto-nakamori@users.noreply.github.com>
2020-10-09 05:27:49 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Form } from 'react-bootstrap';
-import ImgJumbotron from '../assets/jumbotron.jpg'
+import ImgJumbotron from '../assets/joi-shouma.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
